39ba282dbb87183b2d7aa719a8357029a31e6564
sherinanooj <sherin.karuvadan@gmail.com>
2025-03-13 18:23:32 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import logo from "./assets/Images/logo.svg";
+
+function App() {
+  return (
+    <>
+      <h1>Assalamu alaikum Spend sense</h1>
+      <img src={logo} alt="" />
+    </>
+  );
+}
+
+export default App;
