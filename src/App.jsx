d22039ab90b6923d51100e06f40f575942f6d02c
sherinanooj <sherin.karuvadan@gmail.com>
2025-03-14 11:42:00 -0400

    Added pages in folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <h1>Assalamu alaikum Spend sense</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/expense" element={<ExpensePage />} />
+          <Route path="/expense/add" element={<AddExpenseFormPage />} />
+          <Route path="/expense/edit" element={<EditExpensePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
