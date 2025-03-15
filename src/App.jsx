3be1ddec4ce647ee6e41d1499fd0e052e3c43aa2
sherinanooj <sherin.karuvadan@gmail.com>
2025-03-14 20:25:11 -0400

    add footer and header into the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Header from "./components/Header/Header";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
 import ExpensePage from "./pages/ExpensePage/ExpensePage";
 import AddExpenseFormPage from "./pages/AddExpenseFormPage/AddExpenseFormPage";
 import EditExpenseFormPage from "./pages/EditExpenseFormPage/EditExpenseFormPage";
+import Footer from "./components/Footer/Footer";
 import { useState } from "react";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route
             path="/"
@@ -29,6 +32,7 @@ function App() {
             element={<EditExpenseFormPage user={user} />}
           />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer__content">
+        <p className="footer__copyright">
+          &copy; 2025 Spend Sense. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
