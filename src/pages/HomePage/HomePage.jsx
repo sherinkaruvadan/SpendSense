eca1b2eaa8814f7b8a632566a0ed03826e0f1309
sherinanooj <sherin.karuvadan@gmail.com>
2025-03-18 13:36:48 -0400

    create component folder for summary and months drop-down

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./HomePage.scss";
+import Summary from "../../components/Summary/Summary";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="main">
+      <h2>Dashboard</h2>
+      <section className="summary">
+        <Summary />
+      </section>
+    </div>
+  );
 };
 
 export default HomePage;
